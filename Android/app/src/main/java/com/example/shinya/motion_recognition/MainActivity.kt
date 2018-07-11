7e1eb7dc971f1c6fdff95995889242363c2c8915
hiriorima <g2117001@fun.ac.jp>
2018-07-12 02:47:31 +0900

    add dtw

--- Android/app/src/main/java/com/example/shinya/motion_recognition/MainActivity.kt
+++ Android/app/src/main/java/com/example/shinya/motion_recognition/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.shinya.motion_recognition
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val dtw = DTW()
+
+    private lateinit var distanceText: TextView
+
+    val array1 = floatArrayOf(0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f)
+    val array2 = floatArrayOf(0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        distanceText = findViewById(R.id.result)
+
+        val dtwDistance = dtw.compute(array1, array2).distance.toString()
+        distanceText.text = dtwDistance
     }
 }
