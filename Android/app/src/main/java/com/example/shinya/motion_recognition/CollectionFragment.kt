a6db43d18a8fc13dcf5c4f51c3b616ba22e608ce
hiriorima <g2117001@fun.ac.jp>
2018-07-11 20:39:21 +0900

    add edit text

--- Android/app/src/main/java/com/example/shinya/motion_recognition/CollectionFragment.kt
+++ Android/app/src/main/java/com/example/shinya/motion_recognition/CollectionFragment.kt
@@ -1,28 +1,55 @@
 package com.example.shinya.motion_recognition
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.InputType
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 
 
 class CollectionFragment : Fragment() {
+
+    private var motionLabelEditText: EditText? = null
+    private var inputMethodManager: InputMethodManager? = null
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_collection, container, false)
+        setupView(v)
+        setupEditText(v)
+        return v
+    }
+
+    private fun setupView(v: View) {
         v.isFocusableInTouchMode = true
         v.setOnKeyListener { _, keyCode, event ->
-            if ((keyCode == KeyEvent.KEYCODE_VOLUME_UP  || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN )
+            if ((keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
                     && event.action == KeyEvent.ACTION_UP) {
-                // 何らかの処理
+                val label = motionLabelEditText?.text
+
                 true
             } else false
         }
-        return v
+    }
+
+    private fun setupEditText(v: View) {
+        inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        motionLabelEditText = v.findViewById(R.id.motion_label) as EditText
+        motionLabelEditText?.inputType = InputType.TYPE_CLASS_TEXT
+        motionLabelEditText?.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                val inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(v.windowToken, 0)
+                true
+            }
+            false
+        }
     }
 }
